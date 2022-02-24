c307ac047f16d09cb010d37a554525a15438b56d
journy002 <journy002@naver.com>
2022-02-24 18:47:54 +0900

    react 기초 다잡기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Todolist from "./component/todolist";
-import AddForm from "./component/addForm";
+import Counter from "./component/counter";
 
 function App() {
-  const users = [
-    {
-      name: "peter",
-      age: 30,
-      job: "teacher",
-    },
-    {
-      name: "peter2",
-      age: 32,
-      job: "hammer",
-    },
-    {
-      name: "peter3",
-      age: 33,
-      job: "grove",
-    },
-  ];
-
   return (
     <>
-      <AddForm />
-      <Todolist users={users} />
+      <Counter />
     </>
   );
 }
