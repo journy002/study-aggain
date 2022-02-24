216b748976721cc360ae51f92fc4979fb390ffd4
journy002 <journy002@naver.com>
2022-02-24 19:07:12 +0900

    control input state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./component/counter";
+import InputSample from "./component/inputSample";
 
 function App() {
   return (
     <>
-      <Counter />
+      <InputSample />
     </>
   );
 }
